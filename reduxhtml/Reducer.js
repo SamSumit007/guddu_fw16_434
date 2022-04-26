1b88a436f5ef170a63104be0d6f115998f2fbe78
GUDDU434 <99042645+GUDDU434@users.noreply.github.com>
2022-04-26 22:31:56 +0530

    REACT CUSTOM CALCULATOR USING REDUX

--- reduxhtml/Reducer.js
+++ reduxhtml/Reducer.js
@@ -10,6 +10,26 @@ export const reducer = (state, action)=>{
                 ...state,count:state.count--
             }
         }
+        case "ADD":{
+            return {
+                ...state,count:(state.count+=(+action.value))
+            }
+        }
+        case "SUBTRACT":{
+            return {
+                ...state,count:(state.count-=(+action.value))
+            }
+        }
+        case "MULTIPLY":{
+            return {
+                ...state,count:(state.count*=(+action.value))
+            }
+        }
+        case "DIVIDE":{
+            return {
+                ...state,count:(state.count/=(+action.value))
+            }
+        }
         default:{
             return state
         }
--- reduxhtml/index.js
+++ reduxhtml/index.js
@@ -1,17 +1,21 @@
 import Store from "./Store.js";
-
 import { reducer } from "./Reducer.js";
-
-const store = new Store(reducer, {
-  count: 50,
-});
 
 let counter = document.getElementById("counter");
 let inc = document.getElementById("increment");
 let dec = document.getElementById("decrement");
+let add = document.getElementById("add");
+let sub = document.getElementById("subtract");
+let multi = document.getElementById("multiply");
+let divide = document.getElementById("divide");
+let x = document.getElementById("inpValue");
+
+const store = new Store(reducer, {
+  count: 0,
+});
 
 counter.innerText = store.getState().count;
-
+// increment
 inc.addEventListener("click", function () {
   store.dispatch({
     type: "INCREMENT",
@@ -19,9 +23,50 @@ inc.addEventListener("click", function () {
   counter.innerText = store.getState().count;
 });
 
+// decrement
 dec.addEventListener("click", function () {
   store.dispatch({
     type: "DECREMENT",
+  });
+  counter.innerText = store.getState().count;
+});
+
+// Adding
+add.addEventListener("click", function () {
+  let X = x.value;
+  store.dispatch({
+    type: "ADD",
+    value: X,
+  });
+  counter.innerText = store.getState().count;
+});
+
+// subtract
+sub.addEventListener("click", function () {
+  let X = x.value;
+  store.dispatch({
+    type: "SUBTRACT",
+    value: X,
+  });
+  counter.innerText = store.getState().count;
+});
+
+// MULTIPLY
+multi.addEventListener("click", function () {
+  let X = x.value;
+  store.dispatch({
+    type: "MULTIPLY",
+    value: X,
+  });
+  counter.innerText = store.getState().count;
+});
+
+// divide
+divide.addEventListener("click", function () {
+  let X = x.value;
+  store.dispatch({
+    type: "DIVIDE",
+    value: X,
   });
   counter.innerText = store.getState().count;
 });
