691c16f61ef7951f171cda76dfb3b218a8026948
GUDDU434 <99042645+GUDDU434@users.noreply.github.com>
2022-05-16 22:58:42 +0530

    type script assignments

--- unit-5/sprint-1/day-1/index.js
+++ unit-5/sprint-1/day-1/index.js
@@ -12,9 +12,6 @@ function insertionSort(userData, what) {
     for (i = 1; i < userData.length; i++) {
         key = userData[i];
         j = i - 1;
-        /* Move elements of arr[0..i-1], that are
-        greater than key, to one position ahead
-        of their current position */
         while (j >= 0 && userData[j].id > key.id) {
             userData[j + 1] = userData[j];
             j = j - 1;
@@ -23,5 +20,20 @@ function insertionSort(userData, what) {
     }
     return userData;
 }
-var new_data = insertionSort(userData, "salary");
+var new_data = insertionSort(userData, "id");
 console.log(new_data);
+var sub;
+(function (sub) {
+    sub["chemestry"] = "chemestry";
+    sub["physics"] = "physics";
+    sub["math"] = "math";
+})(sub || (sub = {}));
+var studetsData = [
+    { id: 101, name: "nrupul", subject: sub.math, age: 25, no_of_students: 200 },
+    { id: 111, name: "yogesh", subject: sub.physics, age: 45, no_of_students: 250 },
+    { id: 501, name: "Abhishek", subject: sub.chemestry, age: 24, no_of_students: 100 },
+    { id: 201, name: "Shahbaj", subject: sub.math, age: 23, no_of_students: 150 },
+    { id: 212, name: "Prabhanjan", subject: sub.physics, age: 35, no_of_students: 350 }
+];
+var new_studetsData = insertionSort(studetsData, "id");
+console.log(new_studetsData);
--- unit-5/sprint-1/day-1/index.ts
+++ unit-5/sprint-1/day-1/index.ts
@@ -13,11 +13,8 @@ const userData: user[] = [
 {  name: "chandra",  age: 35,  salary: 9000000,  id:211},
 
 ];
-
-
 // const data = userData.sort((a,b)=>a.salary-b.salary)
 // console.log(data);
-
 function insertionSort(userData:user[], what) 
 { 
     let i:number, key, j:number; 
@@ -25,10 +22,6 @@ function insertionSort(userData:user[], what)
     { 
         key = userData[i]; 
         j = i - 1; 
-   
-        /* Move elements of arr[0..i-1], that are 
-        greater than key, to one position ahead 
-        of their current position */
         while (j >= 0 && userData[j].id > key.id)
         { 
             userData[j + 1] = userData[j]; 
@@ -38,9 +31,31 @@ function insertionSort(userData:user[], what)
     } 
     return userData;
 } 
-
 let new_data = insertionSort(userData,"id")
 console.log(new_data);
 
 
+ enum sub {
+  chemestry="chemestry",
+  physics="physics",
+  math="math"
+}
+type teachers ={
+  id:number;
+  name:string;
+  subject?:sub
+  age:number;
+  no_of_students:number
+}
 
+
+const studetsData:teachers[] =[
+{id:101, name:"nrupul",subject:sub.math, age:25, no_of_students:200},
+{id:111, name:"yogesh",subject:sub.physics, age:45, no_of_students:250},
+{id:501, name:"Abhishek",subject:sub.chemestry, age:24, no_of_students:100},
+{id:201, name:"Shahbaj",subject:sub.math, age:23, no_of_students:150},
+{id:212, name:"Prabhanjan",subject:sub.physics, age:35, no_of_students:350}
+]
+
+const new_studetsData=insertionSort(studetsData,"id");
+console.log(new_studetsData)
